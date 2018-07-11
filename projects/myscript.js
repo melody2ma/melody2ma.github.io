e4b51670ab7f73786fc2a23c93b480e22ae06e7c
Melody Ma <melody2ma@gmail.com>
2018-07-11 10:01:34 -0700

    edit

---
+++ projects/myscript.js
@@ -0,0 +1,8 @@
+function changeMayaImage(){
+  document.getElementById('normalMaya').src='/Users/girlswhocode/Downloads/IMG_4081.JPG';
+
+}
+
+function makeMayaNormal(){
+  document.getElementById('explodeMaya').src= '/Users/girlswhocode/Downloads/IMG_4051.JPG';
+}
